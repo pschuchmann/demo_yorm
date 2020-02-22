29573a9e1bec47c5bff176bfd1f230267bbf25fc
Pascal Schuchmann <35096518+pschuchmann@users.noreply.github.com>
2020-02-22 17:02:09 +0100

    run

--- install.php
+++ install.php
@@ -9,5 +9,28 @@ if (file_exists($table_definitions)) {
     rex_yform_manager_table_api::importTablesets($table_data);
 }
 
+//Add Mediapool Files
+$zipArchive = new ZipArchive();
+try {
+    if (true === $zipArchive->open($this->getPath('install/media.zip')) &&
+        true === $zipArchive->extractTo(rex_path::media())) {
+        for ($i = 0; $i < $zipArchive->numFiles; ++$i) {
+            $name = $zipArchive->statIndex($i)['name'];
+
+            if (rex_media::get($name)) {
+                continue;
+            }
+            rex_mediapool_syncFile($name, 0, '');
+        }
+
+        $zipArchive->close();
+    } else {
+        $message = rex_i18n::msg('error_copy_media');
+    }
+} catch (Exception $e) {
+    $message = rex_i18n::msg('error_copy_media');
+    $message .= '<br>' . $e->getMessage();
+}
+
 // clear table cache
 rex_yform_manager_table::deleteCache();
